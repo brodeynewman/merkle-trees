e7af8ade65ec075db784d1625864603904121052
Brodey Newman <brodeynewman@gmail.com>
2021-02-14 17:15:59 -0500

    hardcoding sha256

--- nodejs/src/transaction.js
+++ nodejs/src/transaction.js
@@ -1,9 +1,7 @@
 import crypto from 'crypto';
 
-const DEFAULT_HASH_ALG = 'sha256';
-
 // Hashes data and returns a hex string
-export const createHash = (data, alg = DEFAULT_HASH_ALG) => data && crypto.createHash(alg).update(data.toString()).digest('hex');
+export const createHash = (data) => data && crypto.createHash('sha256').update(data.toString()).digest('hex');
 
 export default class Transaction {
   constructor(value) {
